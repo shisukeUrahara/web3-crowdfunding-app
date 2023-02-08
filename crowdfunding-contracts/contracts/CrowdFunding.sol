8760d3105eb52e6dcbb57eb2743bb5492bcf9d37
AbhishekSagu <abhisheksagu@gmail.com>
2023-02-08 11:28:24 +0530

    added createCampaign function in crowdfunding contract

--- crowdfunding-contracts/contracts/CrowdFunding.sol
+++ crowdfunding-contracts/contracts/CrowdFunding.sol
@@ -2,5 +2,54 @@
 pragma solidity ^0.8.9;
 
 contract CrowdFunding {
-    constructor() {}
+    //  state variables
+    struct Campaign{
+        address owner;
+        string description;
+        string title;
+        uint256 target;
+        uint256 deadline;
+        uint256 amountCollected;
+        string image;
+        address[] donators;
+        uint256[] donations;
+    }
+
+    mapping(uint256=>Campaign) public campaigns;
+    uint256 public campaignCount;
+
+    //  constructor
+
+    //  modifiers
+
+    //  functions
+    function createCampaign(
+        address _owner,
+        string memory title,
+        string memory _description,
+        string memory _image,
+        uint256 _target,
+        uint256 _deadline,
+
+    ) public returns(uint256){
+        Campaign storage newCampaign= campaigns[campaignCount];
+        require(newCampaign.timestamp<block.timestamp,"deadline should be from the future");
+
+        newCampaign.owner=_owner;
+        newCampaign.title=_title;
+        newCampaign.description=_description;
+        newCampaign.image=_image;
+        newCampaign.target=_target;
+        newCampaign.target=_target;
+        newCampaign.amountCollected=0;
+
+        campaignCount++;
+
+        return campaignCount-1;
+
+    };
+
+    function donateToCampaign(){};
+    function getDonators(){};
+    function getCampaigns(){};
 }
