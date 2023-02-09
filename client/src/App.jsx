b2623d558c48acb5e6f12440cbdce0d35e13f247
AbhishekSagu <abhisheksagu@gmail.com>
2023-02-09 18:53:09 +0530

    initial commit for crowdfunding client app

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { ConnectWallet } from "@thirdweb-dev/react";
+// import "./styles/Home.css";
+import './index.css'
+
+export default function Home() {
+  return (
+    <div >
+      <h1>Hello App</h1>
+     
+   
+    </div>
+  );
+}
