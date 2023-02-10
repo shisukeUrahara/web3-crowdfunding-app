9db83fae8e34de3f66a4050c54d0b599e4752d3e
AbhishekSagu <abhisheksagu@gmail.com>
2023-02-10 19:00:12 +0530

    added state context to interact with the contracts

---
+++ client/src/context/index.jsx
@@ -0,0 +1,48 @@
+import React,{useContext,createContext} from 'react';
+import { useAddress,useContract,useMetamask,useContractWrite } from '@thirdweb-dev/react';
+import { ethers } from 'ethers';
+
+
+const StateContext= createContext();
+
+export const StateContextProvider= ({children})=>{
+    const {contract}=useContract('0x63584449b45f3764880f1dffbC487eAECba9959a');
+    const {mutateAsync:createCampaign}=useContractWrite(contract,'createCampaign');
+    const address= useAddress();
+    const connect= useMetamask();
+
+    const publishCampaign= async (form)=>{
+        try{
+            const data = await createCampaign([
+                address,
+                form.title,
+                form.description,
+                form.image,
+                form.target,
+                new Date(form.deadline).getTime()
+            ])
+
+            console.log("**@ campaign creation success , data is , ",data);
+        }
+        catch(err){
+            console.log("**@ campaign creation error , err is , ",err);
+
+
+        }
+        
+
+    }
+
+    return (
+        <StateContext.Provider value={{
+            address,
+            contract,
+            createCampaign:publishCampaign
+        }}>
+            {children}
+
+        </StateContext.Provider>
+    )
+}
+
+export const useStateContext= ()=>useContext(StateContext);
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import { StateContextProvider } from "./context";
 // import "./styles/globals.css";
 
 // This is the chainId your dApp will work on.
@@ -14,7 +15,9 @@ root.render(
   <React.StrictMode>
     <ThirdwebProvider desiredChainId={activeChainId}>
       <BrowserRouter>
-      <App />
+        <StateContextProvider >
+          <App />
+        </StateContextProvider>
       </BrowserRouter>
     </ThirdwebProvider>
   </React.StrictMode>
