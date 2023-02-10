0e701d8e842cf16b71e6b218a9c7d722a9f8636e
AbhishekSagu <abhisheksagu@gmail.com>
2023-02-10 13:44:05 +0530

    added create campaign page

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,9 +1,11 @@
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
 import CustomButton from "./CustomButton";
+import FormField from "./FormField";
 
 export{
     Sidebar,
     Navbar,
-    CustomButton
+    CustomButton,
+    FormField
 }
